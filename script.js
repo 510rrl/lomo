bca9387235923bc603951791899d23179b4aeef2
510rrl <rrlopez@uci.edu>
2024-02-17 23:42:33 -0800

    weather API added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '8df4b5658f790e494a83a8c5be62206e';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
